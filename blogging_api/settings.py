f0de83979465248278a087d501fc70a6fbd085a0
Alvin Elias Ogam Asingo <alvinogam@gmail.com>
2025-12-30 13:58:43 +0300

    Set up filtering criteria

--- blogging_api/settings.py
+++ blogging_api/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "django_filters",
     "categories",
     "tags",
     "posts",
@@ -120,3 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ]
+}
