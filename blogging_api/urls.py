7c99b77160ccf0e3fbefbc96502b40761a9c2f16
Alvin Elias Ogam Asingo <alvinogam@gmail.com>
2025-12-30 13:54:27 +0300

    Modified urls and routes

--- blogging_api/urls.py
+++ blogging_api/urls.py
@@ -19,9 +19,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from posts.views import PostViewSet
+from categories.views import CategoryViewSet
+from tags.views import TagViewSet
 
 router = DefaultRouter()
 router.register(r'posts', PostViewSet)
+router.register(r'categories', CategoryViewSet)
+router.register(r'tags', TagViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
